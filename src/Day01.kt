338cce6a551be8c2df3a82678a5850c372ea52dc
ialokim <ialokim@mailbox.org>
2022-12-02 00:21:05 +0100

    Day :one:

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,14 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+fun parseInput(input: String) = input.split("\n\n").map { it.trim().lines().sumOf { it.toInt() } }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+fun main() {
+    fun part1(input: String) = parseInput(input).maxOf { it }
+
+    fun part2(input: String) = parseInput(input).sortedDescending().take(3).sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
